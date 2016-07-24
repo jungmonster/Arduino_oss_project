cca4ccbdd8d1454e847cefa0d851ef1276851745
minwoo jung <minwoo jung>
2016-07-25 00:03:07 +0900

    Drag & Drop

--- Banana_Project/Banana_Project/MainWindow.xaml.cs
+++ Banana_Project/Banana_Project/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using System.Collections.ObjectModel;
+using System.Collections;
 
 namespace Banana_Project
 {
@@ -21,22 +23,95 @@ namespace Banana_Project
     /// </summary>
     public partial class MainWindow : Window
     {
+        ListBox dragSource = null;
+        ObservableCollection<string> zoneList = new ObservableCollection<string>();
+        ObservableCollection<string> nodeObjectList = new ObservableCollection<string>();
+
         public MainWindow()
         {
             InitializeComponent();
-            NodeScroll.
+            NodeList.MouseDown += NodeListView_MouseDown;
+
+            // Node List 높이를 지정하기 위해 설정... 이후 size가 변화될때마다 새로 설정이 필요함
+            NodeList.Height = this.Height - 70;
+            this.SizeChanged += new SizeChangedEventHandler(MainWindow_SizeChange);
+
+
+            //NodeList.ItemsSource = zoneList;
+
+            SetNodeList();
         }
 
-        private void NodeDockLayer_MouseDown(object sender, MouseButtonEventArgs e)
+        // Node 추가 할 리스트 설정
+        void SetNodeList()
         {
-            Console.WriteLine("mouse : " + sender.ToString());
+            nodeObjectList.Add("Wifi Shield");
+            nodeObjectList.Add("Switch");
+            nodeObjectList.Add("Button");
+            NodeList.ItemsSource = nodeObjectList;
+        }  
+        
+        //  windows size 변화에 따라 ListView Height 변경
+        void MainWindow_SizeChange(object sender, SizeChangedEventArgs e)
+        {
+            NodeList.Height = this.Height - 70;
         }
 
-        private void NodeDock_MouseMove(object sender, MouseEventArgs e)
+        #region Drag&Drop
+        void NodeListView_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            //Console.WriteLine(sender.ToString());
-            //Console.WriteLine("mouse Move : x :" + e.GetPosition(NodeDock).X + ", Y: " + e.GetPosition(NodeDock).Y);
+            Console.WriteLine("Node Click");
+            ListBox parent = (ListBox)sender;
+            dragSource = parent;
+            object data = GetDataFromListView (dragSource, e.GetPosition(parent));
+
+            if (data != null)
+            {
+                DragDrop.DoDragDrop(parent, data, DragDropEffects.Move);
+            }
         }
+        
+
+        void CodeNode_Drop(object sender, DragEventArgs e)
+        {
+            Console.WriteLine("Drop event");
+            ListView parent = (ListView)sender;
+            object data = e.Data.GetData(typeof(string));
+            Console.WriteLine(e.Source.ToString());
+            parent.Items.Add(data);
+        }
+
+        // 마우스 아래 오브젝트 검색
+        private static object GetDataFromListView(ListBox source, Point point)
+        {
+            UIElement element = source.InputHitTest(point) as UIElement;
+            if (element != null)
+            {
+                object data = DependencyProperty.UnsetValue;
+                while (data == DependencyProperty.UnsetValue)
+                {
+                    data = source.ItemContainerGenerator.ItemFromContainer(element);
+
+                    if (data == DependencyProperty.UnsetValue)
+                    {
+                        element = VisualTreeHelper.GetParent(element) as UIElement;
+                    }
+
+                    if (element == source)
+                    {
+                        return null;
+                    }
+                }
+
+                if (data != DependencyProperty.UnsetValue)
+                {
+                    return data;
+                }
+            }
+
+            return null;
+        }
+        #endregion
     }
 
 
---
+++ Banana_Project/Banana_Project/NodeLabel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Banana_Project
+{
+    class NodeLabel : Label
+    {
+        public NodeLabel(string content)
+        {
+            /// init
+            this.Width = 100;
+            this.Height = 30;
+            this.Content = content;
+            setMouseFunction();
+
+        }
+
+        public NodeLabel()
+        {
+            this.Width = 100;
+            this.Height = 30;
+            this.Content = "Content";
+            setMouseFunction();
+        }
+
+        private void setMouseFunction()
+        {
+            this.MouseDown += NodeLabel_MouseDown;
+            this.MouseUp += NodeLabel_MouseUp;
+            //this.MouseMove += NodeLabel_MouseMove;
+        }
+
+        void NodeLabel_MouseDown(object sender,MouseButtonEventArgs e)
+        {
+            Console.WriteLine("Mouse Down");
+        }
+        
+        void NodeLabel_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("Mouse Up");
+        }
+        void NodeLabel_MouseMove(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("Mouse Move");
+        }
+    }
+}
